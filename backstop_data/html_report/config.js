602572e84748ece83f728536eb72c06cf55a2746
Testino <pastamista@gmx.net>
2021-09-18 01:19:25 +0200

    cvcvbcvb

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,167 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_testino_Testino_Home_0_document_0_desktop.png",
+        "test": "..\\bitmaps_test\\20210917-142349\\backstop_testino_Testino_Home_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_testino_Testino_Home_0_document_0_desktop.png",
+        "label": "Testino Home",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:4200/home",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 18.506028163580247,
+          "misMatchPercentage": "18.51",
+          "analysisTime": 90
+        },
+        "diffImage": "..\\bitmaps_test\\20210917-142349\\failed_diff_backstop_testino_Testino_Home_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_testino_Testino_Home_0_document_1_phone.png",
+        "test": "..\\bitmaps_test\\20210917-142349\\backstop_testino_Testino_Home_0_document_1_phone.png",
+        "selector": "document",
+        "fileName": "backstop_testino_Testino_Home_0_document_1_phone.png",
+        "label": "Testino Home",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:4200/home",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 14.590347222222222,
+          "misMatchPercentage": "14.59",
+          "analysisTime": 70
+        },
+        "diffImage": "..\\bitmaps_test\\20210917-142349\\failed_diff_backstop_testino_Testino_Home_0_document_1_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_testino_Testino_Home_0_document_2_tablet.png",
+        "test": "..\\bitmaps_test\\20210917-142349\\backstop_testino_Testino_Home_0_document_2_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_testino_Testino_Home_0_document_2_tablet.png",
+        "label": "Testino Home",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:4200/home",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 10.36669413248698,
+          "misMatchPercentage": "10.37",
+          "analysisTime": 42
+        },
+        "diffImage": "..\\bitmaps_test\\20210917-142349\\failed_diff_backstop_testino_Testino_Home_0_document_2_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_testino_snapshot_h1_0_mat-h1_0_desktop.png",
+        "test": "..\\bitmaps_test\\20210917-142349\\backstop_testino_snapshot_h1_0_mat-h1_0_desktop.png",
+        "selector": ".mat-h1",
+        "fileName": "backstop_testino_snapshot_h1_0_mat-h1_0_desktop.png",
+        "label": "snapshot h1",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:4200/home",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.08556547619047619,
+          "misMatchPercentage": "0.09",
+          "analysisTime": 21
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_testino_snapshot_h1_0_mat-h1_1_phone.png",
+        "test": "..\\bitmaps_test\\20210917-142349\\backstop_testino_snapshot_h1_0_mat-h1_1_phone.png",
+        "selector": ".mat-h1",
+        "fileName": "backstop_testino_snapshot_h1_0_mat-h1_1_phone.png",
+        "label": "snapshot h1",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:4200/home",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.1056985294117647,
+          "misMatchPercentage": "0.11",
+          "analysisTime": 18
+        },
+        "diffImage": "..\\bitmaps_test\\20210917-142349\\failed_diff_backstop_testino_snapshot_h1_0_mat-h1_1_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_testino_snapshot_h1_0_mat-h1_2_tablet.png",
+        "test": "..\\bitmaps_test\\20210917-142349\\backstop_testino_snapshot_h1_0_mat-h1_2_tablet.png",
+        "selector": ".mat-h1",
+        "fileName": "backstop_testino_snapshot_h1_0_mat-h1_2_tablet.png",
+        "label": "snapshot h1",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:4200/home",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.1833545918367347,
+          "misMatchPercentage": "0.18",
+          "analysisTime": 18
+        },
+        "diffImage": "..\\bitmaps_test\\20210917-142349\\failed_diff_backstop_testino_snapshot_h1_0_mat-h1_2_tablet.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_testino"
+});
